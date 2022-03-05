193865985fc648243ebddedfdb83c31dd8187bbf
Ivan Bertona <ivan@ibrt.me>
2022-03-05 18:32:17 -0500

    Add some JSON utilities.

--- jsonz/jsonz.go
+++ jsonz/jsonz.go
@@ -3,6 +3,7 @@ package jsonz
 import (
 	"bytes"
 	"encoding/json"
+
 	"github.com/ibrt/golang-bites/internal"
 )
 
@@ -13,6 +14,11 @@ func MustMarshal(v interface{}) []byte {
 	return buf
 }
 
+// MustMarshalString is like MustMarshal but returns a string.
+func MustMarshalString(v interface{}) string {
+	return string(MustMarshal(v))
+}
+
 // MustMarshalIndent is like json.MarshalIndent but panics on error.
 func MustMarshalIndent(v interface{}, prefix, indent string) []byte {
 	buf, err := json.MarshalIndent(v, prefix, indent)
@@ -20,9 +26,39 @@ func MustMarshalIndent(v interface{}, prefix, indent string) []byte {
 	return buf
 }
 
+// MustMarshalIndentString is like MustMarshalIndent but returns a string.
+func MustMarshalIndentString(v interface{}, prefix, indent string) string {
+	return string(MustMarshalIndent(v, prefix, indent))
+}
+
+// MustMarshalIndentDefault is like MustMarshalIndent with prefix = "" and indent = "  ".
+func MustMarshalIndentDefault(v interface{}) []byte {
+	return MustMarshalIndent(v, "", "  ")
+}
+
+// MustMarshalIndentDefaultString is like MustMarshalIndentDefault with prefix = "" and indent = "  ".
+func MustMarshalIndentDefaultString(v interface{}) string {
+	return MustMarshalIndentString(v, "", "  ")
+}
+
 // MustIndent is like json.Indent but panics on error.
 func MustIndent(src []byte, prefix, indent string) []byte {
 	dst := &bytes.Buffer{}
 	internal.MaybePanic(json.Indent(dst, src, prefix, indent))
 	return dst.Bytes()
+}
+
+// MustIndentString is like MustIndent but returns a string.
+func MustIndentString(src []byte, prefix, indent string) string {
+	return string(MustIndent(src, prefix, indent))
+}
+
+// MustIndentDefault is like MustIndent with prefix = "" and indent = "  ".
+func MustIndentDefault(src []byte) []byte {
+	return MustIndent(src, "", "  ")
+}
+
+// MustIndentDefaultString is like MustIndentString with prefix = "" and indent = "  ".
+func MustIndentDefaultString(src []byte) string {
+	return MustIndentString(src, "", "  ")
 }
--- jsonz/jsonz_test.go
+++ jsonz/jsonz_test.go
@@ -19,6 +19,17 @@ func TestMustMarshal(t *testing.T) {
 	})
 }
 
+func TestMustMarshalString(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustMarshalString(map[string]int{"a": 1})
+		require.Equal(t, `{"a":1}`, buf)
+	})
+
+	require.PanicsWithError(t, `json: unsupported type: func()`, func() {
+		jsonz.MustMarshal(func() {})
+	})
+}
+
 func TestMustMarshalIndent(t *testing.T) {
 	require.NotPanics(t, func() {
 		buf := jsonz.MustMarshalIndent(map[string]int{"a": 1}, "#", "  ")
@@ -30,6 +41,39 @@ func TestMustMarshalIndent(t *testing.T) {
 	})
 }
 
+func TestMustMarshalIndentString(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustMarshalIndentString(map[string]int{"a": 1}, "#", "  ")
+		require.Equal(t, "{\n#  \"a\": 1\n#}", buf)
+	})
+
+	require.PanicsWithError(t, `json: unsupported type: func()`, func() {
+		jsonz.MustMarshalIndentString(func() {}, "#", "  ")
+	})
+}
+
+func TestMustMarshalIndentDefault(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustMarshalIndentDefault(map[string]int{"a": 1})
+		require.Equal(t, "{\n  \"a\": 1\n}", string(buf))
+	})
+
+	require.PanicsWithError(t, `json: unsupported type: func()`, func() {
+		jsonz.MustMarshalIndentDefault(func() {})
+	})
+}
+
+func TestMustMarshalIndentDefaultString(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustMarshalIndentDefaultString(map[string]int{"a": 1})
+		require.Equal(t, "{\n  \"a\": 1\n}", buf)
+	})
+
+	require.PanicsWithError(t, `json: unsupported type: func()`, func() {
+		jsonz.MustMarshalIndentDefaultString(func() {})
+	})
+}
+
 func TestMustIndent(t *testing.T) {
 	require.NotPanics(t, func() {
 		buf := jsonz.MustIndent([]byte(`{"a":1}`), "#", "  ")
@@ -37,6 +81,39 @@ func TestMustIndent(t *testing.T) {
 	})
 
 	require.PanicsWithError(t, `invalid character 'b' looking for beginning of value`, func() {
-		jsonz.MustIndent([]byte(`bad`), "#", "  ")
+		jsonz.MustIndentString([]byte(`bad`), "#", "  ")
+	})
+}
+
+func TestMustIndentString(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustIndentString([]byte(`{"a":1}`), "#", "  ")
+		require.Equal(t, "{\n#  \"a\": 1\n#}", buf)
+	})
+
+	require.PanicsWithError(t, `invalid character 'b' looking for beginning of value`, func() {
+		jsonz.MustIndentString([]byte(`bad`), "#", "  ")
+	})
+}
+
+func TestMustIndentDefault(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustIndentDefault([]byte(`{"a":1}`))
+		require.Equal(t, "{\n  \"a\": 1\n}", string(buf))
+	})
+
+	require.PanicsWithError(t, `invalid character 'b' looking for beginning of value`, func() {
+		jsonz.MustIndentDefault([]byte(`bad`))
+	})
+}
+
+func TestMustIndentDefaultString(t *testing.T) {
+	require.NotPanics(t, func() {
+		buf := jsonz.MustIndentDefaultString([]byte(`{"a":1}`))
+		require.Equal(t, "{\n  \"a\": 1\n}", buf)
+	})
+
+	require.PanicsWithError(t, `invalid character 'b' looking for beginning of value`, func() {
+		jsonz.MustIndentDefaultString([]byte(`bad`))
 	})
 }
